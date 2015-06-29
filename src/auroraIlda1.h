13925b2d272152e1a04c191b910baf9398c83e52
Daniel Tsadok <dtsadok@thoughtworks.com>
2015-06-29 16:37:53 -0400

    updated gitignore (and whitespace)

--- src/auroraIlda1.h
+++ src/auroraIlda1.h
@@ -8,27 +8,27 @@ class auroraIlda1 : public ofBaseApp{
 	public:
     ofxIlda::Frame ildaFrame;
     ofxEtherdream etherdream;
-    
+
     float CenterX, CenterY;
 
     float scale = 1.0;
     ofColor white, color1, color2, color3, color4;
     int currentScene = 0;
-    
+
     const int sceneTimes[5] = {0, 1000, 3000, 6000, 9000};
-    
+
     void setup();
     void update();
     void draw();
-    
+
     //scenes
     int getSceneNumber();
     void drawScene1();
     void drawScene2();
     void drawScene3();
     void drawScene4();
-    
+
     void beat();
-    
+
     void gotMessage(ofMessage msg);
 };
